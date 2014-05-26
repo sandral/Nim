dd84dd9fd9f14da855574861b33fb10821b67b94
Sandra Luhtaniemi <anni.luhtaniemi@helsinki.fi>
2014-05-26 13:17:19 +0300

    muoks

--- Nim/src/main/java/Nim/Stack.java
+++ Nim/src/main/java/Nim/Stack.java
@@ -1,18 +1,34 @@
 package Nim;
 
+/** 
+ * Luokka jonka alkiot ovat kasaolioita, jotka sisältävät tiedon yhden tikkukasan 
+ * tikkumäärästä.
+ */
 public class Stack {
 
     int size;
 
+    /**
+     * Konstruktori
+     * @param size kasaa luotaessa on määriteltävä sen sisältämien tikkujen määrä.
+     */
     public Stack(int size) {
         this.size = size;
     }
 
 
+    /**
+     * Palauttaa kasan sisältämien tikkujen lukumäärän
+     * @return kasan sisältämien tikkujen lukumäärä.
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * Asettaa kasan sisältämien tikkujen lukumääräksi halutuksi.
+     * @param size haluttu lukumäärä
+     */
     public void setSize(int size) {
         this.size = size;
     }
@@ -26,12 +42,19 @@ public class Stack {
         return s;
     }
 
+    /**
+     * Metodi, joka vähentää kasasta yhden tikun.
+     */
     public void decrease() {
         if (!isEmpty()) {
             setSize(getSize() - 1);
         }
      }
 
+    /**
+     * Metodi, joka tarkistaa, onko kasa tyhjä.  
+     * @return totuusarvo, onko kasassa olevien tikkujen määrä 0 eli kasa on tyhjä.
+     */
     public boolean isEmpty() {
         return getSize() == 0;
     }
